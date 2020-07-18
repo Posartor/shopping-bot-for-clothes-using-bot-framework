72605757b30d167abe5992e2c56f171a06ff07eb
Shen Junyang <45538189+john20000625@users.noreply.github.com>
2020-07-18 19:17:46 +0800

    Add files via upload

--- helpers/pointExtract_helper.py
+++ helpers/pointExtract_helper.py
@@ -10,8 +10,8 @@ def pointExtract(text1):
 
 	client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
 
-	looking_ls = ['便宜点','贵一点']
-	name_ls = ["too-expensive","to-cheap"]
+	looking_ls = ['便宜点','贵一点','长袖','短袖','正式职场','休闲','帅潮流酷','甜美可爱']
+	name_ls = ["too-expensive","to-cheap","long-sleeves","short-sleeves","style-office","style-casual","style-cool","style-japanese"]
 
 
 	""" 调用短文本相似度 """
@@ -23,4 +23,17 @@ def pointExtract(text1):
 		result = client.simnet(text1, looking_ls[i])["score"]
 		score_ls.append(result)
 		score_dict[name_ls[i]] = score_ls[i]
-	return score_dict
+	#print("similarity = ")
+	#print(score_dict)
+	max = 0
+	index = 0
+	for i in range(len(score_ls)):
+		if score_ls[i]>max:
+			index = i
+			max = score_ls[i]
+	score_dict_max = {name_ls[index]:max}
+
+	print("score_dict_maxsimilarity = ")
+	print(score_dict_max)
+
+	return score_dict_max
